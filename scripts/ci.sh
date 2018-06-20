6087690496e5acb3ad0a24b94f15c6ecf918669c
Steven Shriver <stshrive@microsoft.com>
2018-06-20 15:11:13 -0700

    Add setup

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,8 +2,8 @@
 
 # Add the scripts directory to the directory stack
 # and set the root path to the scripts directory.
-pushd `dirname "$0"` > /dev/null
-ROOT_PATH=`pwd`
+pushd $(dirname "$0") > /dev/null
+ROOT_PATH=$(pwd)
 popd > /dev/null
 
 # get build versions
---
+++ scripts/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo apt-get update 2>&1
+sudo apt-get install -y --no-install-recommends python3.6
+
