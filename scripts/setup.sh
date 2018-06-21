e3aad661ac4cd67da36d99b295d4245c4e3ef773
Steven Shriver <stshrive@microsoft.com>
2018-06-21 08:32:53 -0700

    Add root path to requirements.txt install command

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,9 +2,16 @@
 
 set -e
 
+# Add the scripts directory to the directory stack
+# and set the root path to the scripts directory.
+pushd $(dirname "$0") > /dev/null
+ROOT_PATH=$(pwd)
+popd > /dev/null
+
+
 sudo apt-get update 2>&1
 sudo apt-get install -y --no-install-recommends python3-pip >&1
 
-sudo -H pip3 -q install  --upgrade pip
-sudo -H pip3 -q install -r requirements.txt
+pip3 -q install  --upgrade pip
+pip3 -q install -r $ROOT_PATH/requirements.txt
 
