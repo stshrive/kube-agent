207e7b47d433b2b3feb27bd0ce8ee9783259b30c
Steven Shriver <stshrive@microsoft.com>
2018-06-21 18:55:14 -0700

    Use parameters in render

--- render.sh
+++ render.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
+if [ $# > 1 ]; then exit 1; fi
+if [ $# < 1 ]; then exit 2; fi
+
+placeholder=$1
+versions=$2
+
+
 render() {
-    sub="s~%%MSFTKUBE_VERSION%%~$version~g;"
+    sub="s~$placeholder~$2~g;"
     sed -r "$sub" $1
 }
 
 #versions=$(echo $MSFTKUBE_VERSIONS | tr ";" " ")
 
-for version in $verions
+for version in $versions
 do
     mkdir $version
     echo rendering dockerfile for version $version
-    render dockerfile.template > $version/dockerfile
+    render dockerfile.template $version > $version/dockerfile
 done
