0110c89884ba7438e943ceeb45ab060019cef04d
Steven Shriver <stshrive@microsoft.com>
2018-06-26 13:01:30 -0700

    Add output file argument

--- render.sh
+++ render.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then 
-    echo requires two arguments
+    echo "$0 requires two arguments [placehoder, versions (file), outputfile]"
     exit 1
 fi
 
 placeholder=$1
 versionfile=$2
+outputfile=$3
 
 render() {
     template=$1
@@ -23,5 +24,9 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
         mkdir $version
         echo rendering dockerfile for version $version
         render dockerfile.template $version > $version/dockerfile
+
+        pushd $(dirname "$0")/$version > /dev/null
+        echo $(pwd)/dockerfile $version >> ../$outputfile
+        popd > /dev/null
     done
 done < $versionfile
