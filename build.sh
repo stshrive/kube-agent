b5d2a6e84b6866b0891d40158863ad3fa441af66
Steven Shriver <stshrive@microsoft.com>
2018-06-26 13:03:10 -0700

    Add build script for multi-image builds

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 3 ]
+then 
+    echo "$0 requires two arguments [Container Registry URL, Container Registry PORT, Dockerfiles (file)]"
+    exit 1
+fi
+
+registry=$1
+registry_port=$2
+dockerfiles=$3
+
+# Builds a docker image
+build() {
+    dockerfile=$1
+    version=$2
+    
+    echo Dockerfile: $dockerfile
+    echo Version: $version
+
+    tag=$registry:$registry_port/kube-agent:$version
+
+    echo Building image $tag
+    docker build . -f $dockerfile -t $tag 
+
+    echo Pushing image $tag
+    docker push $tag
+}
+
+# The file containing the generated dockerfiles gets piped into
+# the loop below.
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    build ${line[@]}
+done < $dockerfiles
