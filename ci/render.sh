21281f3d54ed89147c314b0cb3b2dda4b96f7c83
Steven Shriver <6007067+stshrive@users.noreply.github.com>
2018-06-29 21:36:42 -0700

    Enable multi image builds and tag with msftkube version.
    * add initial dockerignore file
    
    * Clarify arguments
    
    * Add output file argument
    
    * Add comment for loop injection
    
    * Add docker to setup
    
    * Add build script for multi-image builds
    
    * Add wildcard to ignore gitignore file
    
    * Move files to ci dorectory
    
    * remove scripts from docker ignore
    
    * Rename script
    
    * redirect script to proper location
    
    * remove eval statement
    
    * Remove duplicate value check
    
    * Remove vsts parameter
    
    * reword logging message
    
    * fix reference
    
    * rearrange arguments
    
    * fix docker build context
    
    * remove python pip uninstall
    
    * automate docker install
    
    * Fix directory references
    
    * ignore dockerfile template
    
    * fix parameter message
    
    * Remove port parameter
    
    * remove noisy echos
    
    * Add working directory
    
    * use version directory
    
    * add set -e command
    
    * fix root directory
    
    * fix popd command
    
    * add docker login for custom registries
    
    * fix variable expansion
    
    * Fix login
    
    * cat -> echo
    
    * optimize template
    
    * update README
    
    * update README style

---
+++ ci/render.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 3 ]
+then 
+    echo "$0 requires three arguments [placehoder, versions (file), outputfile]"
+    exit 1
+fi
+
+set -e
+
+versionfile=$1
+outputfile=$2
+placeholder=$3
+
+pushd $(dirname "$0") > /dev/null
+WORKING_DIR=$(pwd)
+ROOT_DIR=$(pwd)/..
+popd > /dev/null
+
+render() {
+    template=$1
+    version_number=$2
+
+    sub="s~$placeholder~$version_number~g;"
+    sed -r "$sub" $template
+}
+
+# The file containing the version information gets piped into
+# the loop below.
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    for version in $line
+    do
+        dir=$WORKING_DIR/$version
+        mkdir $dir
+        echo rendering dockerfile for version $version
+        render $ROOT_DIR/dockerfile.template $version > $dir/dockerfile
+
+        pushd $dir > /dev/null
+        echo $(pwd)/dockerfile $version >> $WORKING_DIR/$outputfile
+        popd > /dev/null
+    done
+done < $versionfile
